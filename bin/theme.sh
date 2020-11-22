8b39338502fbe49758ee7817546637f24914345e
a7raser <a7raser@gmail.com>
2020-11-22 14:39:58 +0000

    some, not a lot

--- bin/theme.sh
+++ bin/theme.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wal --iterative -i ~/media/images/wallpapers; \
+wal --iterative -i ~/media/images/wallpapers -o ~/bin/dunst.sh; \
 	COUNT=$(cat $HOME/.fehbg | awk '{print $4}' | wc -c); \
 	COUNT=$(($COUNT-3)); \
 	WALLPAPER=$(cat $HOME/.fehbg | awk '{print $4}' | cut -c2-$COUNT); \
